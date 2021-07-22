b7c934f99f762742ba6f8b584fd28c2a0d771e11
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-07-22 18:12:01 -0400

    fix: fix audio test

--- jinahub/encoders/audio/VGGishAudioEncoder/tests/integration/test_exec.py
+++ jinahub/encoders/audio/VGGishAudioEncoder/tests/integration/test_exec.py
@@ -26,7 +26,7 @@ def test_flow_from_yml():
 
 def test_embedding_exists():
 
-    x_audio, sample_rate = librosa.load(os.path.join(cur_dir, '../data/sample.wav'))
+    x_audio, sample_rate = librosa.load(os.path.join(cur_dir, '../test_data/sample.wav'))
     log_mel_examples = vggish_input.waveform_to_examples(x_audio, sample_rate)
     doc = DocumentArray([Document(blob=log_mel_examples)])
 
