2a7ec17bc54773924bbdbd49cdd6f5ca9d187cbc
Cristian <cristian.mitroi@jina.ai>
2021-07-28 09:44:29 +0200

    ci: remove unnecessary setup.py

--- jinahub/crafters/ImageNormalizer/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="executor-image-normalizer",
-    py_modules=['jinahub.image.normalizer'],
-    package_dir={'jinahub.image': '.'},
-    install_requires=open("requirements.txt").readlines(),
-    python_requires=">=3.7",
-)
