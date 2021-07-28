9258f92a60e2cc05ca5fae255ee2095876eb87e4
Cristian <cristian.mitroi@jina.ai>
2021-07-28 09:21:53 +0200

    fix: add name to setup.py

--- jinahub/crafters/ImageNormalizer/setup.py
+++ jinahub/crafters/ImageNormalizer/setup.py
@@ -1,6 +1,7 @@
 import setuptools
 
 setuptools.setup(
+    name="executor-image-normalizer",
     py_modules=['jinahub.image.normalizer'],
     package_dir={'jinahub.image': '.'},
     install_requires=open("requirements.txt").readlines(),
