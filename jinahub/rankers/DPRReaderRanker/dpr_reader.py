96c60adae7edbc3b91077c59817617e81a9eb1da
Tadej Svetina <tadej.svetina@jina.ai>
2021-08-03 09:43:23 +0200

    fix: simplify batching

--- jinahub/rankers/DPRReaderRanker/dpr_reader.py
+++ jinahub/rankers/DPRReaderRanker/dpr_reader.py
@@ -15,7 +15,7 @@ def _batcher(iterable, n=1):
     """
     l = len(iterable)
     for ndx in range(0, l, n):
-        yield iterable[ndx : min(ndx + n, l)]
+        yield iterable[ndx : ndx + n]
 
 
 def _sort_by(x: List, sort_by: List, reverse: bool = True) -> List:
