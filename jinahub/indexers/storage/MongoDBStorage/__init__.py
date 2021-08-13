832a14c1d934cf77b8bb7eb7b7e88d1ed244fb7f
bwanglzu <bo.wang@jina.ai>
2021-08-13 14:13:14 +0200

    feat: fix imports

--- jinahub/indexers/storage/MongoDBStorage/__init__.py
+++ jinahub/indexers/storage/MongoDBStorage/__init__.py
@@ -1,4 +1,4 @@
 __copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
-from mongo_storage import MongoDBStorage
-from mongo_handler import MongoHandler
+from .mongo_storage import MongoDBStorage
+from .mongo_handler import MongoHandler
