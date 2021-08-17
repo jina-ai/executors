b620b4b80f298bb8c460d5a1aadb64c9a90aa550
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-17 19:06:04 +0200

    test: traversal path

--- jinahub/indexers/merger/SimpleMerger/simple_merger.py
+++ jinahub/indexers/merger/SimpleMerger/simple_merger.py
@@ -1,7 +1,6 @@
 __copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from collections import OrderedDict
 from typing import List, Tuple
 
 from jina import Executor, requests, DocumentArray
@@ -16,25 +15,22 @@ class SimpleMerger(Executor):
     :param kwargs: additional key value arguments
     """
 
-    def __init__(self, default_traversal_paths: Tuple[str] = ('r',), is_unique: bool = False, **kwargs):
-        self.is_unique = is_unique
+    def __init__(self, default_traversal_paths: Tuple[str, ...] = ('r', ), **kwargs):
         self.default_traversal_paths = default_traversal_paths
         super().__init__(**kwargs)
 
     @requests
     def merge(self, docs_matrix: List[DocumentArray], parameters: dict, **kwargs):
         traversal_paths = parameters.get('traversal_paths', self.default_traversal_paths)
-        is_unique = parameters.get('is_unique', self.is_unique)
         results = {}
         for docs in docs_matrix:
-            for doc in docs.traverse_flat(traversal_paths):
-                if doc.id in results:
-                    if is_unique:
-                        for match in doc.matches:
-                            if match not in results[doc.id].matches:
-                                results[doc.id].matches.add(match)
-                    else:
-                        results[doc.id].matches.extend(doc.matches)
-                else:
-                    results[doc.id] = doc
+            self._merge_shard(results, docs, traversal_paths)
         return DocumentArray(list(results.values()))
+
+    def _merge_shard(self, results, docs, traversal_paths):
+        for doc in docs.traverse_flat(traversal_paths):
+            if doc.id in results:
+                results[doc.id].matches.extend(doc.matches)
+            else:
+                results[doc.id] = doc
+
--- jinahub/indexers/merger/SimpleMerger/tests/unit/test_simple_merger.py
+++ jinahub/indexers/merger/SimpleMerger/tests/unit/test_simple_merger.py
@@ -2,36 +2,52 @@ __copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 import pytest
-from jina import Flow, Document
+from jina import Flow, Document, DocumentArray
 
 from jinahub.indexers.merger.SimpleMerger.simple_merger import SimpleMerger
 
 
 @pytest.fixture
 def docs_matrix():
-    docs_matrix = [
-        Document(
-            text=f'doc {i}',
-            matches=[
-                Document(text=f'doc {i}, match {j}')
-                for j in range(3)
-            ],
-            chunks=[
+    return [
+        DocumentArray(
+            [
                 Document(
-                    text=f'doc {i}, chunk {j}',
+                    id=f'doc {i}',
                     matches=[
+                        Document(id=f'doc {i}, match {j}')
+                        for j in range(3)
+                    ],
+                    chunks=[
                         Document(
-                            text=f'doc {i}, chunk {j}, match {k}'
+                            id=f'doc {i}, chunk {j}',
+                            matches=[
+                                Document(
+                                    id=f'doc {i}, chunk {j}, match {k}'
+                                )
+                                for k in range(2)
+                            ]
                         )
-                        for k in range(2)
-                    ]
-                )
-                for j in range(3)
-            ])
-        for i in range(2)
+                        for j in range(3)
+                    ])
+                for i in range(2)
+            ]
+        )
+        for shard in range(4)
     ]
 
 
-def test_simple_merger(docs_matrix):
-    executor = SimpleMerger(default_traversal_paths=('r',))
-    executor.merge(docs_matrix=docs_matrix)
+def test_root_traversal(docs_matrix):
+    executor = SimpleMerger()
+    document_array = executor.merge(docs_matrix=docs_matrix, parameters={})
+    assert len(document_array) == 2
+    for d in document_array:
+        assert len(d.matches) == 12
+
+
+def test_chunk_traversal(docs_matrix):
+    executor = SimpleMerger(default_traversal_paths=('c', ))
+    document_array = executor.merge(docs_matrix=docs_matrix, parameters={})
+    assert len(document_array) == 6
+    for d in document_array:
+        assert len(d.matches) == 8
