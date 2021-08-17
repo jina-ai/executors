3d827df52a4961594b75f344c1939c00847760df
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2021-08-17 10:35:50 -0500

    test: merge unique

--- jinahub/indexers/merger/SimpleMerger/simple_merger.py
+++ jinahub/indexers/merger/SimpleMerger/simple_merger.py
@@ -16,19 +16,25 @@ class SimpleMerger(Executor):
     :param kwargs: additional key value arguments
     """
 
-    def __init__(self, default_traversal_paths: Tuple[str] = ('r',), **kwargs):
-
+    def __init__(self, default_traversal_paths: Tuple[str] = ('r',), is_unique: bool = False, **kwargs):
+        self.is_unique = is_unique
         self.default_traversal_paths = default_traversal_paths
         super().__init__(**kwargs)
 
     @requests
-    def merge(self, docs_matrix, parameters: dict, **kwargs):
+    def merge(self, docs_matrix: List[DocumentArray], parameters: dict, **kwargs):
         traversal_paths = parameters.get('traversal_paths', self.default_traversal_paths)
+        is_unique = parameters.get('is_unique', self.is_unique)
         results = {}
         for docs in docs_matrix:
             for doc in docs.traverse_flat(traversal_paths):
                 if doc.id in results:
-                    results[doc.id].matches.extend(doc.matches)
+                    if is_unique:
+                        for match in doc.matches:
+                            if match not in results[doc.id].matches:
+                                results[doc.id].matches.add(match)
+                    else:
+                        results[doc.id].matches.extend(doc.matches)
                 else:
                     results[doc.id] = doc
         return DocumentArray(list(results.values()))
