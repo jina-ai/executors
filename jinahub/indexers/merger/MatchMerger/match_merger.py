6f34bd9c5518258ba60ddc1a835460c680e7db8f
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-20 17:21:54 +0200

    feat: simple merger (#102)
    * feat: simple merger
    
    * refactor: restructure files
    
    * test: merge unique
    
    * test: traversal path
    
    * refactor: move lines
    
    * refactor: rename merger
    
    * refactor: renaming
    
    * test: add requirement
    
    * refactor: restructure
    
    * refactor: rename
    
    * refactor: move lines
    
    * docs: remove alias
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * docs: remove author
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * docs: remove documentation
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * docs: adjust url
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * refactor: remove git
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    Co-authored-by: Sergiy Chemodanov <sergiychemodanov@gmail.com>
    Co-authored-by: Nan Wang <nan.wang@jina.ai>

---
+++ jinahub/indexers/merger/MatchMerger/match_merger.py
@@ -0,0 +1,35 @@
+__copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+from typing import List, Tuple
+
+from jina import Executor, requests, DocumentArray
+
+
+class MatchMerger(Executor):
+    """
+    The MatchMerger merges the results of shards by appending all matches..
+
+    :param default_traversal_paths: traverse path on docs, e.g. ['r'], ['c']
+    :param args: additional arguments
+    :param kwargs: additional key value arguments
+    """
+
+    def __init__(self, default_traversal_paths: Tuple[str, ...] = ('r',), **kwargs):
+        super().__init__(**kwargs)
+        self.default_traversal_paths = default_traversal_paths
+
+    @requests
+    def merge(self, docs_matrix: List[DocumentArray], parameters: dict, **kwargs):
+        traversal_paths = parameters.get('traversal_paths', self.default_traversal_paths)
+        results = {}
+        for docs in docs_matrix:
+            self._merge_shard(results, docs, traversal_paths)
+        return DocumentArray(list(results.values()))
+
+    def _merge_shard(self, results, docs, traversal_paths):
+        for doc in docs.traverse_flat(traversal_paths):
+            if doc.id in results:
+                results[doc.id].matches.extend(doc.matches)
+            else:
+                results[doc.id] = doc
