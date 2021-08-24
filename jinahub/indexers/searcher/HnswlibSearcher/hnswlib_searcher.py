621d539e62f536ad69bfb4916c5ee2db12da9e4b
Joan Fontanals <joan.martinez@jina.ai>
2021-08-24 14:34:20 +0200

    fix: catch error fill_embedding (#126)
    Co-authored-by: felix-wang <35718120+numb3r3@users.noreply.github.com>

--- jinahub/indexers/searcher/HnswlibSearcher/hnswlib_searcher.py
+++ jinahub/indexers/searcher/HnswlibSearcher/hnswlib_searcher.py
@@ -111,6 +111,10 @@ class HnswlibSearcher(Executor):
         if docs is None:
             return
         for doc in docs:
-            doc.embedding = np.array(
-                self._indexer.get_items([int(self._doc_id_to_offset[str(doc.id)])])[0]
-            )
+            doc_idx = self._doc_id_to_offset.get(doc.id)
+            if doc_idx is not None:
+                doc.embedding = np.array(
+                    self._indexer.get_items([int(doc_idx)])[0]
+                )
+            else:
+                self.logger.warning(f'Document {doc.id} not found in index')
