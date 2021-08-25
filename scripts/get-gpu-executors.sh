f655a8120f4b581500d923514ae6a4cbbc499473
jakob1111996 <jakob.kruse@jina.ai>
2021-08-25 13:17:58 +0200

    refactor: parallelize docker gpu testing

---
+++ scripts/get-gpu-executors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# find all the examples with changed code
+# run the tests in that directory
+set -ex
+
+containers=()
+gpu_folders=()
+root=`pwd`
+
+for changed_file in $CHANGED_FILES; do
+
+  file_base_dir=$(dirname $changed_file)
+  if [ $(basename $file_base_dir) = "tests" ]; then
+    file_base_dir=$(dirname "$file_base_dir")
+  fi
+  cd $file_base_dir
+
+  if [[ ! " ${changed_folders[@]} " =~ " ${file_base_dir} " ]]; then
+    if [[ $file_base_dir != "." ]]; then
+      if [[ -f "Dockerfile" ]]
+        echo "GPU executor found in " $file_base_dir
+        gpu_folders+=(${file_base_dir})
+      }
+    fi
+  fi
+  cd $root
+done
+
+#echo will store gpu_folders in output matrix
+echo "::set-output name=matrix::$(printf '%s\n' "${gpu_folders[@]}" | jq -R . | jq -cs .)"
