b6c33e689326d1ac7888ce32aaa27ef66047aa78
Tadej Svetina <tadej.svetina@jina.ai>
2021-08-26 00:24:48 +0200

    fix: remove redundant code

--- jinahub/encoders/text/AudioCLIPTextEncoder/tests/conftest.py
+++ jinahub/encoders/text/AudioCLIPTextEncoder/tests/conftest.py
@@ -12,10 +12,7 @@ from jina import Document, DocumentArray
 @pytest.fixture(scope="session", autouse=True)
 def download_cache():
     subprocess.run(
-        'scripts/download_full.sh',
-        cwd=Path(__file__).parents[1],
-        check=True,
-        stdout=subprocess.DEVNULL,
+        'scripts/download_full.sh', cwd=Path(__file__).parents[1], check=True
     )
     yield
     shutil.rmtree('.cache')
