7efe0464cb4798f4a4f9e9a98dcf23852b95649b
Tadej Svetina <tadej.svetina@jina.ai>
2021-08-26 15:24:47 +0200

    fix: minimal fixes to unit tests to pass

--- jinahub/encoders/text/DPRTextEncoder/dpr_text.py
+++ jinahub/encoders/text/DPRTextEncoder/dpr_text.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Literal, Optional
+from typing import Iterable, Optional
 
 import torch
 from jina import DocumentArray, Executor, requests
@@ -45,7 +45,7 @@ class DPRTextEncoder(Executor):
     def __init__(
         self,
         pretrained_model_name_or_path: str = 'facebook/dpr-question_encoder-single-nq-base',
-        encoder_type: Literal['context', 'question'] = 'question',
+        encoder_type: str = 'question',
         base_tokenizer_model: Optional[str] = None,
         title_tag_key: Optional[str] = None,
         max_length: Optional[int] = None,
