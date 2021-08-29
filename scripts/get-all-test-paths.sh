402fb3a48230b5358e76366df5532b8519608dc0
Tadej <tadej.svetina@gmail.com>
2021-08-29 15:44:22 +0200

    fix: debug

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -7,6 +7,8 @@ changed_folders=()
 
 for changed_file in $CHANGED_FILES; do
   file_base_dir=$(dirname $changed_file)
+  echo $changed_file
+  echo $file_base_dir
 
   # only if the folder has a tests or a Dockerfile but excluding integration tests (always run & separate)
   if [[ -f "${file_base_dir}/Dockerfile" || -d "${file_base_dir}/tests/" ]]; then
