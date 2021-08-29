e63f8462c24a25aa998d2029d7dd6b48e0a92881
Tadej <tadej.svetina@gmail.com>
2021-08-29 15:52:40 +0200

    fix: debug

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -7,18 +7,11 @@ changed_folders=()
 
 for changed_file in $CHANGED_FILES; do
   file_base_dir=$(dirname $changed_file)
-  echo $changed_file
-  echo "+++++++++++++++++++++++++++++++++++++++"
-  echo "${file_base_dir}/Dockerfile"
-  echo "+++++++++++++++++++++++++++++++++++++++"
 
   # only if the folder has a tests or a Dockerfile but excluding integration tests (always run & separate)
   if [[ -f "${file_base_dir}/Dockerfile" || -d "${file_base_dir}/tests/" ]]; then
-    echo "step 1"
     if [[ ! " ${changed_folders[@]} " =~ " ${file_base_dir} " ]]; then
-      echo "step 2"
       if [[ $file_base_dir != "." ]]; then
-        echo "adding $file_base_dir"
         changed_folders+=(${file_base_dir})
       fi
     fi
