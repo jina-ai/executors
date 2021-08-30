17f862590cdd1dc8302cace3d007e0bdfba0a288
jakob1111996 <jakob.kruse@jina.ai>
2021-08-30 13:11:22 +0200

    ci: notice changes in test folders

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -7,6 +7,14 @@ changed_folders=()
 
 for changed_file in $CHANGED_FILES; do
   file_base_dir=$(dirname $changed_file)
+  # Test folder changes
+  if [ $(basename $file_base_dir) = "tests" ]; then
+    file_base_dir=$(dirname "$file_base_dir")
+  fi
+  # Changes in subfolder of test folder (e.g. unit_test/integration)
+  if [ $(basename $(dirname "$file_base_dir")) = "tests" ]; then
+    file_base_dir=$(dirname $(dirname "$file_base_dir"))
+  fi
 
   # only if the folder has a tests or a Dockerfile but excluding integration tests (always run & separate)
   if [[ -f "${file_base_dir}/Dockerfile" || -d "${file_base_dir}/tests/" ]]; then
