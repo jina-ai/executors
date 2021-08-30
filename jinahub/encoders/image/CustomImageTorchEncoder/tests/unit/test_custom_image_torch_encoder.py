b5c31e366075f44270d228f3b9a7321949107c77
jakob1111996 <jakob.kruse@jina.ai>
2021-08-30 16:14:30 +0200

    feat: custom image torch encoder

--- jinahub/encoders/image/CustomImageTorchEncoder/tests/unit/test_custom_image_torch_encoder.py
+++ jinahub/encoders/image/CustomImageTorchEncoder/tests/unit/test_custom_image_torch_encoder.py
@@ -49,3 +49,26 @@ def test_encoder_traversal_paths(encoder):
     assert len(docs.traverse_flat(['c'])) == 4
     for chunk in docs.traverse_flat(['c']):
         assert chunk.embedding.shape == (output_dim,)
+
+
+@pytest.mark.gpu
+def test_encoder_gpu():
+    model_dir = Path(__file__).parents[1] / 'model'
+    encoder = CustomImageTorchEncoder(
+        model_definition_file=str(model_dir / 'external_model.py'),
+        model_state_dict_path=str(model_dir / 'model_state_dict.pth'),
+        layer_name='conv1',
+        model_class_name='ExternalModel',
+        device='cuda',
+    )
+    output_dim = 10
+    input_dim = 224
+    test_img = np.random.rand(3, input_dim, input_dim)
+    docs = DocumentArray([Document(blob=test_img), Document(blob=test_img)])
+    encoder.encode(docs, {})
+    import time
+
+    time.sleep(5)
+    assert len(docs) == 2
+    for doc in docs:
+        assert doc.embedding.shape == (output_dim,)
