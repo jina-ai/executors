ec7a7ec15ec41f23ea7f03d80b6ae6ccb81265e4
Maximilian Werk <maximilian.werk@jina.ai>
2021-08-30 10:42:17 +0200

    feat: added missing endpoints

--- jinahub/indexers/searcher/compound/FaissPostgresSearcher/faisspsql.py
+++ jinahub/indexers/searcher/compound/FaissPostgresSearcher/faisspsql.py
@@ -11,7 +11,9 @@ from jina_commons import get_logger
 try:
     from jinahub.indexers.searcher.FaissSearcher import FaissSearcher
 except:
-    from jina_executors.indexers.searcher.FaissSearcher.faiss_searcher import FaissSearcher
+    from jina_executors.indexers.searcher.FaissSearcher.faiss_searcher import (
+        FaissSearcher,
+    )
 
 try:
     from jinahub.indexers.storage.PostgreSQLStorage import (
@@ -33,16 +35,16 @@ class FaissPostgresSearcher(Executor):
     ):
         super().__init__(**kwargs)
         # when constructed from rolling update the dump_path is passed via a runtime_arg
-        dump_path = dump_path or kwargs.get('runtime_args').get('dump_path')
+        self._init_kwargs = kwargs
+        dump_path = dump_path or self._init_kwargs.get('runtime_args').get('dump_path')
         self.logger = get_logger(self)
-        self._kv_indexer = None
-        self._vec_indexer = None
+        self._kv_indexer = PostgreSQLStorage(**self._init_kwargs)
         if dump_path:
-            self._vec_indexer = FaissSearcher(dump_path=dump_path, **kwargs)
-            self._kv_indexer = PostgreSQLStorage(**kwargs)
+            self._vec_indexer = FaissSearcher(dump_path=dump_path, **self._init_kwargs)
         else:
+            self._vec_indexer = None
             self.logger.warning(
-                f'No dump path provided for {self}. Use .rolling_update() to re-initialize...'
+                f'No dump path provided for {self}. Use /reload to re-initialize...'
             )
 
     @requests(on='/search')
@@ -55,4 +57,35 @@ class FaissPostgresSearcher(Executor):
             ]
             self._kv_indexer.search(docs, kv_parameters)
         else:
+            self.logger.warning(
+                'Not all sub-indexers initialized. Use /reload to re-initialize...'
+            )
+
+    @requests(on='/dump')
+    def dump(self, parameters: Dict, **kwargs):
+        """Dump the index
+
+        :param parameters: a dictionary containing the parameters for the dump
+        """
+        self._kv_indexer.dump(parameters, **kwargs)
+
+    @requests(on='/reload')
+    def reload(self, parameters: Dict, **kwargs):
+        dump_path = parameters.get('dump_path', None)
+        if dump_path is None:
+            self.logger.error(f'No "dump_path" provided for {self}')
             return
+
+        self._vec_indexer = FaissSearcher(dump_path=dump_path, **self._init_kwargs)
+
+    @requests(on='/index')
+    def index(self, **kwargs):
+        self._kv_indexer.add(**kwargs)
+
+    @requests(on='/update')
+    def update(self, **kwargs):
+        self._kv_indexer.update(**kwargs)
+
+    @requests(on='/delete')
+    def delete(self, **kwargs):
+        self._kv_indexer.delete(**kwargs)
