1581227f1cd98284d81da74cfdfc3f257f0de64d
Tadej Svetina <tadej.svetina@jina.ai>
2021-09-14 11:25:40 +0200

    feat: refactor text executors pt 2 (#175)

--- jinahub/encoders/text/SpacyTextEncoder/README.md
+++ jinahub/encoders/text/SpacyTextEncoder/README.md
@@ -2,9 +2,13 @@
 
 **SpacyTextEncoder** is a class that encodes text with spaCy models.
 
+#### Inputs 
 
+`Document` with `text`.
 
+#### Returns
 
+`Document` with `embedding` field filled with spacy vector.
 
 ## Usage 
 
@@ -18,15 +22,6 @@ with f:
     resp = f.post(on='foo', inputs=Document(), return_results=True)
 	print(f'{resp}')
 ```
-
-
-#### Inputs 
-
-`Document` with `text`.
-
-#### Returns
-
-`Document` with `embedding` field filled with spacy vector.
 
 ## Reference
 - https://spacy.io/models/
--- jinahub/encoders/text/TransformerTorchEncoder/tests/unit/test_encoder.py
+++ jinahub/encoders/text/TransformerTorchEncoder/tests/unit/test_encoder.py
@@ -7,36 +7,38 @@ import torch
 from jina import Document, DocumentArray, Executor
 
 from ...transform_encoder import TransformerTorchEncoder
-from ..integration.test_integration import filter_none
+
+_EMBEDDING_DIM = 768
+
+
+@pytest.fixture(scope='session')
+def basic_encoder() -> TransformerTorchEncoder:
+    return TransformerTorchEncoder()
 
 
 def test_config():
     ex = Executor.load_config(str(Path(__file__).parents[2] / 'config.yml'))
-    assert (
-        ex.pretrained_model_name_or_path
-        == 'sentence-transformers/distilbert-base-nli-stsb-mean-tokens'
+    assert ex.pooling_strategy == 'mean'
+
+
+def test_compute_tokens(basic_encoder: TransformerTorchEncoder):
+    tokens = basic_encoder._generate_input_tokens(
+        ['hello this is a test', 'and another test']
     )
-
-
-def test_compute_tokens():
-    enc = TransformerTorchEncoder()
-
-    tokens = enc._generate_input_tokens(['hello this is a test', 'and another test'])
 
     assert tokens['input_ids'].shape == (2, 7)
     assert tokens['attention_mask'].shape == (2, 7)
 
 
 @pytest.mark.parametrize('hidden_seqlen', [4, 8])
-def test_compute_embeddings(hidden_seqlen):
+def test_compute_embeddings(hidden_seqlen: int, basic_encoder: TransformerTorchEncoder):
     embedding_size = 10
-    enc = TransformerTorchEncoder()
-    tokens = enc._generate_input_tokens(['hello world'])
+    tokens = basic_encoder._generate_input_tokens(['hello world'])
     hidden_states = tuple(
         torch.zeros(1, hidden_seqlen, embedding_size) for _ in range(7)
     )
 
-    embeddings = enc._compute_embedding(
+    embeddings = basic_encoder._compute_embedding(
         hidden_states=hidden_states, input_tokens=tokens
     )
 
@@ -49,7 +51,7 @@ def test_encoding_cpu():
 
     enc.encode(docs=input_data, parameters={})
 
-    assert input_data[0].embedding.shape == (768,)
+    assert input_data[0].embedding.shape == (_EMBEDDING_DIM,)
 
 
 @pytest.mark.gpu
@@ -59,88 +61,97 @@ def test_encoding_gpu():
 
     enc.encode(docs=input_data, parameters={})
 
-    assert input_data[0].embedding.shape == (768,)
-
-
-def test_encodes_semantic_meaning():
-    sentences = dict()
-    sentences['A'] = 'Hello, my name is Michael.'
-    sentences['B'] = 'Today we are going to Disney World.'
-    sentences['C'] = 'There are animals on the road'
-    sentences['D'] = 'A dog is running down the road'
-
-    encoder = TransformerTorchEncoder()
-
-    embeddings = {}
-    for id_, sentence in sentences.items():
-        docs = DocumentArray([Document(text=sentence)])
-        encoder.encode(docs, parameters={})
-        embeddings[id_] = docs[0].embedding
-
-    def dist(a, b):
-        a_embedding = embeddings[a]
-        b_embedding = embeddings[b]
-        return np.linalg.norm(a_embedding - b_embedding)
-
-    small_distance = dist('C', 'D')
-    assert small_distance < dist('C', 'B')
-    assert small_distance < dist('C', 'A')
-    assert small_distance < dist('B', 'A')
+    assert input_data[0].embedding.shape == (_EMBEDDING_DIM,)
 
 
 @pytest.mark.parametrize(
-    ['docs', 'docs_per_path', 'traversal_path'],
+    'model_name',
     [
-        (pytest.lazy_fixture('docs_with_text'), [['r', 10], ['c', 0], ['cc', 0]], 'r'),
-        (
-            pytest.lazy_fixture('docs_with_chunk_text'),
-            [['r', 0], ['c', 10], ['cc', 0]],
-            'c',
-        ),
-        (
-            pytest.lazy_fixture('docs_with_chunk_chunk_text'),
-            [['r', 0], ['c', 0], ['cc', 10]],
-            'cc',
-        ),
+        'microsoft/deberta-base',
+        'distilbert-base-uncased',
+        'mrm8488/longformer-base-4096-finetuned-squadv2',
+    ],
+)
+def test_models(model_name: str):
+    encoder = TransformerTorchEncoder(model_name)
+    input_data = DocumentArray([Document(text='hello world')])
+
+    encoder.encode(docs=input_data, parameters={})
+
+    assert input_data[0].embedding.shape == (_EMBEDDING_DIM,)
+
+
+@pytest.mark.parametrize('layer_index', [0, 1, -1])
+def test_layer_index(layer_index: int):
+    encoder = TransformerTorchEncoder(layer_index=layer_index)
+    input_data = DocumentArray([Document(text='hello world')])
+    encoder.encode(docs=input_data, parameters={})
+
+    assert input_data[0].embedding.shape == (_EMBEDDING_DIM,)
+
+
+@pytest.mark.parametrize('pooling_strategy', ['cls', 'mean', 'min', 'max'])
+def test_pooling_strategy(pooling_strategy: str):
+    encoder = TransformerTorchEncoder(pooling_strategy=pooling_strategy)
+    input_data = DocumentArray([Document(text='hello world')])
+
+    encoder.encode(input_data, parameters={})
+    assert input_data[0].embedding.shape == (_EMBEDDING_DIM,)
+
+
+@pytest.mark.parametrize(
+    'traversal_paths, counts',
+    [
+        (['r'], [['r', 1], ['c', 0], ['cc', 0]]),
+        (['c'], [['r', 0], ['c', 3], ['cc', 0]]),
+        (['cc'], [['r', 0], ['c', 0], ['cc', 2]]),
+        (['cc', 'r'], [['r', 1], ['c', 0], ['cc', 2]]),
     ],
 )
 def test_traversal_path(
-    docs: DocumentArray, docs_per_path: List[List[str]], traversal_path: str
+    traversal_paths: List[str], counts: List, basic_encoder: TransformerTorchEncoder
 ):
-    def validate_traversal(expected_docs_per_path: List[List[str]]):
-        def validate(res):
-            for path, count in expected_docs_per_path:
-                embeddings = filter_none(
-                    DocumentArray(res).traverse_flat([path]).get_attributes('embedding')
-                )
-                for emb in embeddings:
-                    if emb is None:
-                        return False
-                assert len(embeddings) == count
+    text = 'blah'
+    docs = DocumentArray([Document(id='root1', text=text)])
+    docs[0].chunks = [
+        Document(id='chunk11', text=text),
+        Document(id='chunk12', text=text),
+        Document(id='chunk13', text=text),
+    ]
+    docs[0].chunks[0].chunks = [
+        Document(id='chunk111', text=text),
+        Document(id='chunk112', text=text),
+    ]
 
-        return validate
-
-    encoder = TransformerTorchEncoder(default_traversal_paths=[traversal_path])
-    encoder.encode(docs, {'traversal_paths': [traversal_path]})
-
-    validate_traversal(docs_per_path)(docs)
+    basic_encoder.encode(docs=docs, parameters={'traversal_paths': traversal_paths})
+    for path, count in counts:
+        embeddings = docs.traverse_flat([path]).get_attributes('embedding')
+        assert len(list(filter(lambda x: x is not None, embeddings))) == count
 
 
-def test_multiple_traversal_paths():
-    sentences = list()
-    sentences.append('Hello, my name is Michael.')
-    sentences.append('Today we are going to Disney World.')
-    sentences.append('There are animals on the road')
-    sentences.append('A dog is running down the road')
-    docs = DocumentArray([Document(text=sentence) for sentence in sentences])
-    for index, sent in enumerate(sentences):
-        docs[index].chunks.append(Document(text=sent))
-        docs[index].chunks[0].chunks.append(Document(text=sentences[3 - index]))
+@pytest.mark.parametrize('batch_size', [1, 2, 4, 8])
+def test_batch_size(basic_encoder: TransformerTorchEncoder, batch_size: int):
+    docs = DocumentArray([Document(text='hello there') for _ in range(32)])
+    basic_encoder.encode(docs, parameters={'batch_size': batch_size})
 
-    encoder = TransformerTorchEncoder(default_traversal_paths=['r', 'c', 'cc'])
-
-    encoder.encode(docs, {})
     for doc in docs:
-        assert doc.embedding.shape == (768,)
-        assert doc.chunks[0].embedding.shape == (768,)
-        assert doc.chunks[0].chunks[0].embedding.shape == (768,)
+        assert doc.embedding.shape == (_EMBEDDING_DIM,)
+
+
+def test_quality_embeddings(basic_encoder: TransformerTorchEncoder):
+    docs = DocumentArray(
+        [
+            Document(id='A', text='a furry animal that with a long tail'),
+            Document(id='B', text='a domesticated mammal with four legs'),
+            Document(id='C', text='a type of aircraft that uses rotating wings'),
+            Document(id='D', text='flying vehicle that has fixed wings and engines'),
+        ]
+    )
+
+    basic_encoder.encode(DocumentArray(docs), {})
+
+    # assert semantic meaning is captured in the encoding
+    docs.match(docs)
+    matches = ['B', 'A', 'D', 'C']
+    for i, doc in enumerate(docs):
+        assert doc.matches[1].id == matches[i]
