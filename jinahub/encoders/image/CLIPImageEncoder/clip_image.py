cbabfa2007854b0ba7298196eb06b121ff004f3c
Tadej Svetina <tadej.svetina@jina.ai>
2021-09-15 14:34:25 +0200

    feat: improve clip encoder documentation (#194)

--- jinahub/encoders/image/CLIPImageEncoder/clip_image.py
+++ jinahub/encoders/image/CLIPImageEncoder/clip_image.py
@@ -8,27 +8,7 @@ from transformers import CLIPFeatureExtractor, CLIPModel
 
 
 class CLIPImageEncoder(Executor):
-    """
-    Encode image into embeddings.
-
-    :param pretrained_model_name_or_path: Can be either:
-        - A string, the model id of a pretrained CLIP model hosted
-            inside a model repo on huggingface.co, e.g., 'openai/clip-vit-base-patch32'
-        - A path to a directory containing model weights saved, e.g.,
-            ./my_model_directory/
-    :param base_feature_extractor: Base feature extractor for images.
-        Defaults to ``pretrained_model_name_or_path`` if None
-    :param use_default_preprocessing: Whether to use the `base_feature_extractor` on
-        images (blobs) before encoding them. If you disable this, you must ensure
-        that the images you pass in have the correct format, see the ``encode`` method
-        for details.
-    :param device: device that the model is on (should be "cpu", "cuda" or "cuda:X",
-        where X is the index of the GPU on the machine)
-    :param default_batch_size: fallback batch size in case there is no batch size
-        sent in the request
-    :param default_traversal_paths: fallback traversal path in case there is no
-        traversal path sent in the request
-    """
+    """Encode image into embeddings using the CLIP model."""
 
     def __init__(
         self,
@@ -36,14 +16,32 @@ class CLIPImageEncoder(Executor):
         base_feature_extractor: Optional[str] = None,
         use_default_preprocessing: bool = True,
         device: str = "cpu",
-        default_batch_size: int = 32,
-        default_traversal_paths: Tuple = ("r",),
+        batch_size: int = 32,
+        traversal_paths: Tuple = ("r",),
         *args,
         **kwargs,
     ):
+        """
+        :param pretrained_model_name_or_path: Can be either:
+        - A string, the model id of a pretrained CLIP model hosted
+            inside a model repo on huggingface.co, e.g., 'openai/clip-vit-base-patch32'
+        - A path to a directory containing model weights saved, e.g.
+            `./my_model_directory/`
+        :param base_feature_extractor: Base feature extractor for images.
+            Defaults to ``pretrained_model_name_or_path`` if None
+        :param use_default_preprocessing: Whether to use the `base_feature_extractor` on
+            images (blobs) before encoding them. If you disable this, you must ensure
+            that the images you pass in have the correct format, see the ``encode``
+            method for details.
+        :param device: Pytorch device to put the model on, e.g. 'cpu', 'cuda', 'cuda:1'
+        :param traversal_paths: Default traversal paths for encoding, used if
+            the traversal path is not passed as a parameter with the request.
+        :param batch_size: Default batch size for encoding, used if the
+            batch size is not passed as a parameter with the request.
+        """
         super().__init__(*args, **kwargs)
-        self.default_batch_size = default_batch_size
-        self.default_traversal_paths = default_traversal_paths
+        self.default_batch_size = batch_size
+        self.default_traversal_paths = traversal_paths
         self.pretrained_model_name_or_path = pretrained_model_name_or_path
         self.use_default_preprocessing = use_default_preprocessing
         self.base_feature_extractor = (
@@ -62,23 +60,23 @@ class CLIPImageEncoder(Executor):
     @requests
     def encode(self, docs: Optional[DocumentArray], parameters: dict, **kwargs):
         """
-        Encode all docs with images and store the encodings in the embedding
-        attribute of the docs.
+        Encode all Documents with images (stored in the `blob` attribute) and store the
+        embeddings in the `embedding` attribute of the Documents.
 
-        :param docs: documents sent to the encoder. The docs must have `blob` of the
+        :param docs: Documents sent to the encoder. The docs must have `blob` of the
             shape ``Height x Width x 3``. By default, the input ``blob`` must
             be an ``ndarray`` with ``dtype=uint8`` or ``dtype=float32``.
             The ``Height`` and ``Width`` can have arbitrary values.
 
             If you set ``use_default_preprocessing=True`` when creating this encoder,
-            then the image arrays should have the shape ``[H, W, C]``, and be in the
+            then the image arrays should have the shape ``[H, W, 3]``, and be in the
             RGB color format.
 
             If you set ``use_default_preprocessing=False`` when creating this encoder,
             then you need to ensure that the images you pass in are already
             pre-processed. This means that they are all the same size (for batching) -
             the CLIP model was trained on ``224 x 224`` images, and that they are of
-            the shape ``[C, H, W]`` (in the RGB color format). They should also be
+            the shape ``[3 H, W]`` (in the RGB color format). They should also be
             normalized.
         :param parameters: A dictionary that contains parameters to control encoding.
             The accepted keys are ``traversal_paths`` and ``batch_size`` - in their
