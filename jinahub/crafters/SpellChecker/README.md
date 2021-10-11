0288ab1fbadd2638b6046bc6a01a0360c3bc5915
Cristian <cristian.mitroi@jina.ai>
2021-10-11 16:51:02 +0200

    docs: add more docs

--- jinahub/crafters/SpellChecker/README.md
+++ jinahub/crafters/SpellChecker/README.md
@@ -4,15 +4,35 @@ A simple [BKTree](https://en.wikipedia.org/wiki/BK-tree)-based spelling correcto
 
 ## Basic usage
 
-Requires a custom training dataset. 
-This can be provided by training it locally and then mounting it in your Executor's workspace.
-
-TODO 
-
-Then the spelling of your text Documents can be handled as follows:
+Requires a training dataset of sentences.
 
 ```python
+input_training_data = [
+        'they can go quite fast',
+        'there were the new Japanese Honda',
+    ]
+
+train_docs = DocumentArray([
+    Document(content=t) for t in input_training_data
+    ])
+
+with Flow().add(uses=SpellChecker) as f:
+    f.post(on='/train', inputs=train_docs)
 
 ```
+
+Then the spelling of your text Documents can be fixed as follows:
+
+```python
+...
+input_docs = DocumentArray(
+            [Document(content=t) for t in incorrect_text]
+            )
+results = f.post(on='/index', inputs=input_docs, return_results=True)
+print(results[0].docs)  # documents can be found here
+
+```
+
+Note that calling the `/train` again will delete the existing model.
 
 <!-- version=v0.1 -> 
--- jinahub/crafters/SpellChecker/spell_checker.py
+++ jinahub/crafters/SpellChecker/spell_checker.py
@@ -13,18 +13,34 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 class SpellChecker(Executor):
+    """A simple spell checker based on BKTree
+
+    It can be trained on your own corpus, on the /train endpoint
+
+    Otherwise it automatically spell corrects your Documents with string contents. The content is overridden.
+    """
+
     def __init__(
         self,
         model_path: str = os.path.join(cur_dir, 'model.pickle'),
         traversal_paths: Iterable = ['r'],
+        min_freq: int = 0,
         *args,
         **kwargs,
     ):
+        """
+        :param model_path: the path where the model will be saved
+        :param traversal_paths: the path to traverse docs when processed
+        :param min_freq: the minimum frequency in order to
+            count a word in the corpus
+        """
         super().__init__(*args, **kwargs)
         self.traversal_paths = traversal_paths
+        self.min_freq = min_freq
         self.logger = JinaLogger(self.metas.name)
 
         self.model_path = model_path
+        self.model = None
 
         if os.path.exists(self.model_path):
             with open(self.model_path, 'rb') as model_file:
@@ -34,14 +50,21 @@ class SpellChecker(Executor):
 
     @requests(on='/train')
     def train(self, docs: DocumentArray, parameters: Dict = {}, **kwargs):
-        speller = PyNgramSpell(min_freq=0)
+        """
+        Re-train the BKTree model
+        """
+        self.model = PyNgramSpell(min_freq=parameters.get("min_freq", self.min_freq))
         input_training_data = [d.content for d in docs]
-        speller.fit(input_training_data)
-        speller.save(self.model_path)
+        self.model.fit(input_training_data)
+        self.model.save(self.model_path)
 
     @requests(on=['/index', '/search', '/update', '/delete'])
     def spell_check(self, docs: DocumentArray, parameters: Dict = {}, **kwargs):
+        """
+        Processes the text Documents
+        """
         for d in docs.traverse_flat(
             parameters.get('traversal_paths', self.traversal_paths)
         ):
-            d.content = self.model.transform(d.content)
+            if isinstance(d.content, str):
+                d.content = self.model.transform(d.content)
