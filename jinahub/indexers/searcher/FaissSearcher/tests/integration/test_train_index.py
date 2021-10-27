eac6fa3fd2f077f87645125d90c5faf13227db28
numb3r3 <wangfelix87@gmail.com>
2021-10-27 15:36:26 +0800

    fix: integrate test

--- jinahub/indexers/searcher/FaissSearcher/tests/integration/test_train_index.py
+++ jinahub/indexers/searcher/FaissSearcher/tests/integration/test_train_index.py
@@ -8,7 +8,6 @@ import numpy as np
 import pytest
 from jina import Document, DocumentArray, Flow
 from jina.executors.metas import get_default_metas
-from jina_commons.indexers.dump import export_dump_streaming
 
 from ...faiss_searcher import FaissSearcher
 
@@ -31,49 +30,24 @@ def metas(tmpdir):
     del os.environ['TEST_WORKSPACE']
 
 
-def test_train_and_index(metas, tmpdir):
-    vec_idx = np.random.randint(0, high=512, size=[512]).astype(str)
-    vec = np.array(np.random.random([512, 10]), dtype=np.float32)
-
+def test_train_and_index(metas):
     query = np.array(np.random.random([10, 10]), dtype=np.float32)
     query_docs = _get_docs_from_vecs(query)
 
-    train_data_file = os.path.join(os.environ['TEST_WORKSPACE'], 'train.npy')
-    train_data = np.array(np.random.random([1024, 10]), dtype=np.float32)
-    np.save(train_data_file, train_data)
-
-    trained_index_file = os.path.join(os.environ['TEST_WORKSPACE'], 'faiss.index')
-
-    export_dump_streaming(
-        os.path.join(tmpdir, 'dump'),
-        1,
-        len(vec_idx),
-        zip(vec_idx, vec, [b'' for _ in range(len(vec))]),
-    )
-    dump_path = os.path.join(tmpdir, 'dump')
+    train_data = np.array(np.random.random([512, 10]), dtype=np.float32)
+    train_docs = _get_docs_from_vecs(train_data)
 
     f = Flow().add(
         uses=FaissSearcher,
         timeout_ready=-1,
         uses_with={
             'index_key': 'IVF10_HNSW32,PQ2',
-            'trained_index_file': trained_index_file,
         },
     )
     with f:
-        # the trained index will be dumped to "faiss.index"
-        f.post(on='/train', parameters={'train_data_file': train_data_file})
+        # train and index docs first
+        f.post(on='/train', data=train_docs, request_size=0)
 
-    f = Flow().add(
-        uses=FaissSearcher,
-        timeout_ready=-1,
-        uses_with={
-            'index_key': 'IVF10_HNSW32,PQ2',
-            'trained_index_file': trained_index_file,
-            'dump_path': dump_path,
-        },
-    )
-    with f:
         result = f.post(
             on='/search', data=query_docs, return_results=True, parameters={'top_k': 4}
         )[0].docs
