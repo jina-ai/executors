3da921b8eb9de0c740b58454519345807b9418bf
numb3r3 <wangfelix87@gmail.com>
2021-11-11 14:40:50 +0800

    fix: training with different index_key

--- tests/integration/faiss_psql/test_train_psql.py
+++ tests/integration/faiss_psql/test_train_psql.py
@@ -20,10 +20,11 @@ def get_documents(nr=10, index_start=0, emb_size=256):
 
 
 @pytest.mark.parametrize('docker_compose', [compose_yml], indirect=['docker_compose'])
-def test_oneline_train(docker_compose):
+@pytest.mark.parametrize('index_key', ['IVF64,PQ32', 'IVF8,PQ8', 'HNSW32'])
+def test_online_train(docker_compose, index_key):
     docs = get_documents(10240)
     with Flow().add(
-        uses='FaissPostgresIndexer', uses_with={'index_key': 'IVF64,PQ32'}
+        uses='FaissPostgresIndexer', uses_with={'index_key': index_key}
     ) as f:
         f.post(on='/index', inputs=docs)
         f.post(on='/train')
@@ -34,7 +35,7 @@ def test_oneline_train(docker_compose):
 
 
 @pytest.mark.parametrize('docker_compose', [compose_yml], indirect=['docker_compose'])
-def test_oneline_train_2_flows(docker_compose):
+def test_online_train_2_flows(docker_compose):
     docs = get_documents(10240)
     with Flow().add(
         uses='FaissPostgresIndexer', uses_with={'index_key': 'IVF64,PQ32'}
@@ -52,7 +53,7 @@ def test_oneline_train_2_flows(docker_compose):
 
 
 @pytest.mark.parametrize('docker_compose', [compose_yml], indirect=['docker_compose'])
-def test_oneline_train_3_flows(docker_compose):
+def test_online_train_3_flows(docker_compose):
     docs = get_documents(10240)
     with Flow().add(
         uses='FaissPostgresIndexer', uses_with={'index_key': 'IVF64,PQ32'}
