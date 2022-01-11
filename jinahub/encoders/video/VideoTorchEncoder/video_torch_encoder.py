5dd0903574216c7e04a2222def3cb266545d496a
bwanglzu <bo.wang@jina.ai>
2022-01-11 11:45:44 +0100

    refactor: remove unused imports

--- jinahub/encoders/video/VideoTorchEncoder/video_torch_encoder.py
+++ jinahub/encoders/video/VideoTorchEncoder/video_torch_encoder.py
@@ -1,7 +1,7 @@
 __copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional
 
 import torch
 import torch.nn as nn
